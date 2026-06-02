38dee326efff42746ac5031af0ae86d822916287
Sakura <sakura@example.local>
2026-06-02 12:50:04 +0800

    Make main branch a demo hub index

--- run_server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\Users\Sakura\Desktop\demo
-"D:\python\python.exe" "C:\Users\Sakura\Desktop\demo\app.py" > "C:\Users\Sakura\Desktop\demo\server.out.log" 2> "C:\Users\Sakura\Desktop\demo\server.err.log"
